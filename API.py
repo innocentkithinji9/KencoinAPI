387519a16c92e950454747fad61c30c01e417938
innocentkithinji9 <innocentkithinji9@gmail.com>
2019-06-11 13:08:10 +0300

    130806112019
    withdrawal and show transactions pending

--- API.py
+++ API.py
@@ -13,7 +13,7 @@ from flask_restplus import Api, Resource, fields
 from pywallet import wallet
 from web3 import Web3
 from requests.auth import HTTPBasicAuth
-import parser
+import json as Jay
 
 import keys
 from abi import data
@@ -78,8 +78,19 @@ class CreateWallet(Resource):
 
         w = wallet.create_wallet(network=self.network, seed=seed, children=3)
         childInfo = w["children"][0]
-        wallet_info = {"address": childInfo['address'], "public key": childInfo['xpublic_key'].decode(),
+        wallet_info = {"address": childInfo['address'], "pubkey": childInfo['xpublic_key'].decode(),
                        "seed": w['seed']}
+        return wallet_info
+
+    def get(self):
+        seed = wallet.generate_mnemonic()
+        encrypt = base64.b64encode(seed.encode('utf-8'))
+        print(encrypt)
+        w = wallet.create_wallet(network=self.network, seed=seed, children=3)
+        child = w["children"][0]
+        childInfo = w["children"][0]
+        wallet_info = {"address": childInfo['address'], "pubkey": childInfo['xpublic_key'].decode(),
+                       "seed": w['seed'], "encrypted": encrypt.decode()}
         return wallet_info
 
 
@@ -106,14 +117,8 @@ class CheckBalance(Resource):
 
     def get(self, userAddr):
         print(self.contract)
-        if self.web3.isConnected():
-            if self.web3.isAddress(userAddr):
-                totalsupply = self.contract.functions.balanceOf(userAddr).call()
-                return "{}".format(self.web3.fromWei(totalsupply, 'ether')), 200
-            else:
-                return "Invalid address Provided", 422
-        else:
-            return "This ain't right"
+
+        return getBalance(userAddr)
 
 
 @coinNS.route('/send')
@@ -155,12 +160,13 @@ class SendCoins(Resource):
 
 
 depositModel = coinNS.model("depositModel",
-                            {"phoneNumber": fields.String(description="Number to charge for the deposit eg 254700******",
-                                                           required=True),
-                             "account": fields.String(description="Number to Receive the tokens 0700******",
-                                                                required=True),
-                             "amount": fields.Integer(description="Amount to deposit", required=True)
-                             })
+                            {"phoneNumber": fields.String(
+                                description="Number to charge for the deposit eg 254700******",
+                                required=True),
+                                "account": fields.String(description="Number to Receive the tokens 0700******",
+                                                         required=True),
+                                "amount": fields.Integer(description="Amount to deposit", required=True)
+                            })
 
 
 @coinNS.route("/deposit")
@@ -206,19 +212,105 @@ class MpesaDeposit(Resource):
 
 userNS = api.namespace("user", description="All about users")
 
+userModel = userNS.model("RegistrationModel", {
+    "name": fields.String(required="true", description="Name of User"),
+    "email": fields.String(required="true", description="email of User"),
+    "dob": fields.String(required="true", description="dob of User"),
+    "phone": fields.String(required="true", description="phone of User"),
+    "gender": fields.String(required="true", description="gender of User"),
+    "id": fields.String(required="true", description="id of User"),
+    "pin": fields.String(description="pin of User"),
+    "PPic": fields.String(required="true", description="PPic of User"),
+    "IDFrontPic": fields.String(required="true", description="IDFrontPic of User"),
+    "IDBackPic": fields.String(required="true", description="IDBackPic of User"),
+    "mnemonic": fields.String(description="IDFrontPic of User"),
+    "encrypted": fields.String(required="true", description="encrypted mnemonic of User"),
+    "address": fields.String(required="true", description="wallet addr of User"),
+    "pubKey": fields.String(required="true", description="public Key of User"),
+    "uid": fields.String(required="true", description="User uid"),
+})
+
+
 @userNS.route("/<uid>")
 class User(Resource):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.user_Data_list = []
+        self.url = "https://rinkeby.infura.io/v3/c5aa372b19484e00ad066119a24c646e"
+        self.web3 = Web3(Web3.HTTPProvider(self.url))
 
     def get(self, uid):
         print("Hello:", uid)
         userDocs = db.collection(u'users').where(u'uid', u'==', u'{}'.format(uid)).get()
         for user in userDocs:
             self.user_Data_list.append(user.to_dict())
+        if len(self.user_Data_list) != 0:
+            userDetails = self.user_Data_list[0]
+            usrAddr = self.web3.toChecksumAddress(userDetails['address'])
+            print(usrAddr)
+            balance = getBalance(usrAddr)
+            userDetails['balance'] = balance
+            return userDetails
+        else:
+            return {"new_User": True}
 
-        return self.user_Data_list[0]
+
+@userNS.route("/register")
+class Register(Resource):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user_Data_list = []
+        self.url = "https://rinkeby.infura.io/v3/c5aa372b19484e00ad066119a24c646e"
+        self.web3 = Web3(Web3.HTTPProvider(self.url))
+        self.abi = data
+        self.address = self.web3.toChecksumAddress("0x068540764de212447eeaf9928cde4218fee204d7")
+        self.contract = self.web3.eth.contract(address=self.address, abi=self.abi)
+
+    @userNS.expect(userModel)
+    def post(self):
+        rawdata = flask.request.data.decode()
+        print(rawdata)
+        data = ast.literal_eval(rawdata)
+        print(data['name'])
+        userDocs = db.collection(u'users').where(u'uid', u'==', u'{}'.format(data['uid'])).get()
+        for user in userDocs:
+            self.user_Data_list.append(user.to_dict())
+        if len(self.user_Data_list) == 0:
+            db.collection(u'users').add({
+                u'name': u'{}'.format(data['name']),
+                u'email': u'{}'.format(data['email']),
+                u'dob': u'{}'.format(data['dob']),
+                u'phone': u'{}'.format(data['phone']),
+                u'gender': u'{}'.format(data['gender']),
+                u'uid': u'{}'.format(data['uid']),
+                u'id': u'{}'.format(data['id']),
+                u'pin': u'{}'.format(data['pin']),
+                u'PPic': u'{}'.format(data['PPic']),
+                u'IDFrontPic': u'{}'.format(data['IDFrontPic']),
+                u'mnemonic': u'{}'.format(data['encrypted']),
+                u'address': u'{}'.format(data['address']),
+                u'pubKey': u'{}'.format(data['pubKey']),
+            })
+            return "Successful"
+        else:
+            return "The user is already registered", 304
+
+
+def getBalance(userAddr):
+    url = "https://rinkeby.infura.io/v3/c5aa372b19484e00ad066119a24c646e"
+    web3 = Web3(Web3.HTTPProvider(url))
+    abi = data
+    address = web3.toChecksumAddress("0x068540764de212447eeaf9928cde4218fee204d7")
+    contract = web3.eth.contract(address=address, abi=abi)
+    if web3.isConnected():
+        if web3.isAddress(userAddr):
+            totalsupply = contract.functions.balanceOf(userAddr).call()
+            return "{}".format(web3.fromWei(totalsupply, 'ether')), 200
+        else:
+            return "Invalid address Provided", 422
+    else:
+        return "This ain't right"
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=4000)
